d77c44d3d6063d3adac1c78a75aaae410d813278
Randy Whitley <randywhitley84@gmail.com>
2021-02-22 11:48:35 -0600

    added code manifest.webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 let CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-var urlsToCache = [
+let urlsToCache = [
   "/",
   "/db.js",
   "/index.js",
--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, "mongodb://localhost/budget"  {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
+
 // routes
 app.use(require("./routes/api.js"));
 
